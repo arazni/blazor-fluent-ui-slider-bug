97cab6af0cd0fb3ac70b5bb8e434389a04aba511
Erica Abenti <emmert.math@gmail.com>
2024-02-05 12:26:10 -0600

    demo

---
+++ Model.cs
@@ -0,0 +1,14 @@
+﻿namespace SliderTest;
+
+public class Model
+{
+	public int DataValue { get; set; }
+
+	public const int MaxValue = 3;
+
+	public void Reset()
+	{
+		DataValue = 0;
+		Console.WriteLine("Data Value: " + DataValue);
+	}
+}
